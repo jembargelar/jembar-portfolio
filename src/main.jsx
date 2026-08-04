e1bfdeeddac90ea4af010709df2d0b6f3ceeab04
Jembar Gelar Kusumah Wibawa <jembargelar@gmail.com>
2026-08-04 16:38:23 +0700

    fix: remove AOS library import to resolve blank screen crash

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// IMPORT AOS
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-// IMPORT i18n UNTUK DUA BAHASA 👇
+// IMPORT i18n DUA BAHASA
 import './i18n';
-
-// INISIALISASI AOS
-AOS.init({
-  duration: 1000, // durasi animasi
-  easing: 'ease-in-out', // efek gerakan
-  once: true, // animasi hanya jalan sekali saat scroll
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
